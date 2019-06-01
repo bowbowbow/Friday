88a8ce722cd69697bccdffbc145c0679a210359e
bowbowbow <clsrn1581@gmail.com>
2019-06-02 03:13:40 +0900

    add addSelector to global object

--- browser_extension/chrome/extension/background/contextMenus.js
+++ browser_extension/chrome/extension/background/contextMenus.js
@@ -1,18 +1,23 @@
 const CONTEXT_MENU_ID = 'FRIDAY';
 let windowId = 0;
 
+const addSelector = tabId =>
+  chrome.tabs.executeScript(tabId, {
+    code: 'window.__gs && window.__gs.addSelector()',
+  });
+
 chrome.contextMenus.create({
   id: CONTEXT_MENU_ID,
-  title: 'Friday',
+  title: 'Add this element to candidate',
   contexts: ['all'],
   documentUrlPatterns: [
     'http://*/*',
     'https://*/*',
   ],
-  onclick: e => {
+  onclick: (e, tab) => {
     if (e.menuItemId !== CONTEXT_MENU_ID) {
       return;
     }
-    // copySelector(selectedTabId);
+    addSelector(tab.id);
   },
 });
