33343adc448ebd6558795c50da64f7f8aea50162
bowbowbow <clsrn1581@gmail.com>
2019-06-02 02:58:05 +0900

    simplify contextMenus

--- browser_extension/chrome/extension/background/contextMenus.js
+++ browser_extension/chrome/extension/background/contextMenus.js
@@ -1,29 +1,5 @@
+const CONTEXT_MENU_ID = 'FRIDAY';
 let windowId = 0;
-const CONTEXT_MENU_ID = 'Friday-context-menu';
-
-function closeIfExist() {
-  if (windowId > 0) {
-    chrome.windows.remove(windowId);
-    windowId = chrome.windows.WINDOW_ID_NONE;
-  }
-}
-
-function popWindow(type) {
-  closeIfExist();
-  const options = {
-    type: 'popup',
-    left: 100,
-    top: 100,
-    width: 800,
-    height: 475,
-  };
-  if (type === 'open') {
-    options.url = 'window.html';
-    chrome.windows.create(options, (win) => {
-      windowId = win.id;
-    });
-  }
-}
 
 chrome.contextMenus.create({
   id: CONTEXT_MENU_ID,
@@ -33,11 +9,10 @@ chrome.contextMenus.create({
     'http://*/*',
     'https://*/*',
   ],
-});
-
-chrome.contextMenus.onClicked.addListener((event) => {
-  console.log('event :', event);
-  // if (event.menuItemId === CONTEXT_MENU_ID) {
-  //   popWindow('open');
-  // }
+  onclick: e => {
+    if (e.menuItemId !== CONTEXT_MENU_ID) {
+      return;
+    }
+    // copySelector(selectedTabId);
+  },
 });
