345aeaed0510d2ea589d16a4ffcbaeade5d4ca04
bowbowbow <clsrn1581@gmail.com>
2019-06-03 21:58:56 +0900

    add flask server

---
+++ server.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask import request, Response, g
+import json
+
+from language_parser import get_api_daemon_object, api_call_main
+
+app = Flask(__name__)
+
+
+@app.route('/text2selenium', methods=['POST'])
+def text2selenium():
+    data = request.get_json()
+    text = data['text']
+    selectors = data['selectors']
+
+    run_selenium, use_corenlp, pathsaver, nlp, allennlp = get_api_daemon_object()
+
+    sample = {
+        'text': text,
+        'selectors': selectors,
+    }
+    code = api_call_main(sample, pathsaver, run_selenium, nlp, allennlp)
+    print(code)
+
+    return json.dumps({'code': codd})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=False, port=8081)
