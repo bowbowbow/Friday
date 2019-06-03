59bae6d1a8e1efe074865444fa4592936f069b98
bowbowbow <clsrn1581@gmail.com>
2019-06-04 01:48:47 +0900

    add text2Selenium api on extension

--- chrome_extension/src/models/app.js
+++ chrome_extension/src/models/app.js
@@ -1,8 +1,9 @@
 import queryString from 'query-string';
 import pathToRegexp from 'path-to-regexp';
-import { routerRedux } from 'dva/router';
+import {routerRedux} from 'dva/router';
 import _ from 'lodash';
 import * as chromeAPI from '../utils/chromeAPI';
+import * as service from '../services/app';
 
 export default {
   namespace: 'app',
@@ -12,9 +13,9 @@ export default {
     warning: '',
   },
   subscriptions: {
-    setupHistory({ dispatch, history }) {
+    setupHistory({dispatch, history}) {
       chromeAPI.getState().then((state) => {
-        dispatch({ type: 'updateState', payload: state });
+        dispatch({type: 'updateState', payload: state});
       });
       chrome.runtime.onMessage.addListener(
         (request, sender, sendResponse) => {
@@ -22,13 +23,13 @@ export default {
           return true; // for calling sendResponse asynchronously
         },
       );
-      history.listen(({ pathname, search }) => {
+      history.listen(({pathname, search}) => {
 
       });
     },
   },
   effects: {
-    * sendInitState({ payload }, { put, call, select }) {
+    * sendInitState({payload}, {put, call, select}) {
       const app = yield select(state => state.app);
       const nextState = {
         power: app.power,
@@ -37,9 +38,18 @@ export default {
       };
       yield call(chromeAPI.sendInitState, nextState);
     },
+    * postText2Selenium({payload}, {put, call}) {
+      const res = yield call(service.postText2selenium, payload);
+      if (res.status === 200) {
+        const code = res.body.code;
+        yield put({type: 'updateState', payload: {code}});
+      } else {
+
+      }
+    },
   },
   reducers: {
-    updateState(state, { payload }) {
+    updateState(state, {payload}) {
       const nextState = {
         ...state,
         ...payload,
