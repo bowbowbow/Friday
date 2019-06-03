6c0d40e245a79bc130c7f3aa4d85afbe1db66287
bowbowbow <clsrn1581@gmail.com>
2019-06-03 13:09:34 +0900

    add messaging between contents script and background

--- browser_extension/chrome/extension/inject/index.js
+++ browser_extension/chrome/extension/inject/index.js
@@ -2,18 +2,20 @@ import { init, toggle } from './app';
 
 !(() => {
   const global = window.__fd = window.__fd || {};
-
   console.log('[GetSelector]: Injected');
+
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log('runtime onMessage :', message);
     const action = message.action;
     const data = message.data;
 
-    if (action === 'init') {
-      init(global);
-      toggle(global);
-    } else if (action === 'stop') {
-      toggle(global);
+    if (action === 'init_state') {
+      if (data.state.power) {
+        init(global, data.state);
+        toggle(global);
+      } else {
+        toggle(global);
+      }
     }
   });
 })();
