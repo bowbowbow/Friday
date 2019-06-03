7ce0572b7a5b96d12b61a543385946cd3418e736
ddehun <ddehun@kaist.ac.kr>
2019-06-03 19:45:36 +0900

    sample code

--- output/testfile_0.py
+++ output/testfile_0.py
@@ -1,14 +1,23 @@
 from selenium import webdriver
 
-Input = 'Enter the "KAIST" in "SearchBox" and click the "Search" button'
+Input = 'Open the "https://google.com" and Enter the "Iron man" in #1 and click the #2. Wait the "3 seconds" and Check if "Robert Downey" is on the page'
 
 driver_path = "./../dat/chromedriver.exe"
 driver = webdriver.Chrome(driver_path)
-driver.implicitly_wait(3)
+driver.implicitly_wait(1)
+
+# open the "https://google.com" .
 driver.get('https://google.com')
 
-# enter the "KAIST" in "SearchBox" .
-driver.find_element_by_name('q').send_keys('KAIST')
+# enter the "Iron man" in #1 .
+driver.find_element_by_css_selector('.gLFyf').send_keys('Iron man')
 
-# click the "Search" button .
-driver.find_element_by_name("btnK").click()
+# click the #2 .
+driver.find_element_by_css_selector("center:nth-child(1) > .gNO89b").click()
+
+# wait the "3 seconds" .
+import time
+time.sleep(3)
+
+# check if "Robert Downey" is on the page .
+assert "Robert Downey" in driver.page_source
