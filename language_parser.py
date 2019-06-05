19da8f1521a6acbea7fcb93120c4d0f41304b758
ddehun <ddehun@kaist.ac.kr>
2019-06-05 14:21:30 +0900

    quote of sample

--- language_parser.py
+++ language_parser.py
@@ -234,7 +234,7 @@ def api_main():
           "tagId": 2
         }
       ],
-        "text": 'Open the \'https://google.com\' and sleep the 3 seconds.'}]
+        "text": 'Open the \'https://google.com\' and sleep the "3" seconds.'}]
 
     run_selenium, use_corenlp, pathsaver ,nlp, allennlp = get_api_daemon_object()
 
